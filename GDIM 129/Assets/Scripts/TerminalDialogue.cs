3ed4b7772ef6796cad7639734022673bfa222c27
Jack <jackfisher.hsoi@gmail.com>
2025-06-05 05:36:47 -0700

    Area 1 Done

--- GDIM 129/Assets/Scripts/TerminalDialogue.cs
+++ GDIM 129/Assets/Scripts/TerminalDialogue.cs
@@ -1,20 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class TerminalDialogue : MonoBehaviour
 {
+    [Header("UI Components")]
     [SerializeField] private TextMeshProUGUI textComponent;
-    [SerializeField] private string[] dialogueLines;
-    [SerializeField] private float textDisplaySpeed;
-    [SerializeField] private int index;
 
-    [Space]
+    [TextArea(2, 5)]
+    [SerializeField] private string[] dialogueLines;
+
+    [SerializeField] private float textDisplaySpeed = 0.05f;
+
+    [Header("Dialogue State")]
+    private int index = 0;
+
+    [Header("References")]
     [SerializeField] private PlayerMovement playerObject;
     [SerializeField] private PlayerCam cameraObject;
 
     private PauseMenu pauseMenuObject;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource startAudioSource;
+    [SerializeField] private AudioSource endAudioSource;
+    [SerializeField] private float fadeDuration = 1.0f;
 
     private void Start()
     {
@@ -29,11 +39,17 @@ public class TerminalDialogue : MonoBehaviour
         playerObject.isDialogueOn = true;
         cameraObject.isDialogueOn = true;
 
+        if (startAudioSource != null)
+        {
+            startAudioSource.volume = 0f;
+            startAudioSource.Play();
+            StartCoroutine(FadeAudio(startAudioSource, 1f, fadeDuration));
+        }
+
         StartDialogue();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && !pauseMenuObject.isPaused)
         {
@@ -51,8 +67,7 @@ public class TerminalDialogue : MonoBehaviour
 
     private void StartDialogue()
     {
-        index = 0;
-        if (this.gameObject.activeSelf)
+        if (dialogueLines.Length > 0 && gameObject.activeSelf)
         {
             StartCoroutine(TypeLine());
         }
@@ -60,7 +75,8 @@ public class TerminalDialogue : MonoBehaviour
 
     private IEnumerator TypeLine()
     {
-        foreach (char c in dialogueLines[index].ToCharArray())
+        textComponent.text = string.Empty;
+        foreach (char c in dialogueLines[index])
         {
             textComponent.text += c;
             yield return new WaitForSeconds(textDisplaySpeed);
@@ -77,9 +93,41 @@ public class TerminalDialogue : MonoBehaviour
         }
         else
         {
+            if (endAudioSource != null)
+            {
+                endAudioSource.volume = 1f;
+                endAudioSource.Play();
+                StartCoroutine(FadeAudio(endAudioSource, 0f, fadeDuration));
+            }
+
             playerObject.isDialogueOn = false;
             cameraObject.isDialogueOn = false;
-            gameObject.SetActive(false);
+
+            StartCoroutine(DeactivateAfterSound(fadeDuration));
         }
+    }
+
+    private IEnumerator DeactivateAfterSound(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        gameObject.SetActive(false);
+    }
+
+    private IEnumerator FadeAudio(AudioSource source, float targetVolume, float duration)
+    {
+        float startVolume = source.volume;
+        float time = 0f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            source.volume = Mathf.Lerp(startVolume, targetVolume, time / duration);
+            yield return null;
+        }
+
+        source.volume = targetVolume;
+
+        if (targetVolume == 0f)
+            source.Stop();
     }
 }
